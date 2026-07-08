812044c112301d79b46d5fa0800388ce42a93207
Wiltania <wiltania.pierre-louis@escola.pr.gov.br>
2026-07-08 20:14:04 +0000

    AULA 9

--- main.js
+++ main.js
@@ -87,7 +87,6 @@ function classificaSenha(tamanhoAlfabeto){
     }
 }
 
-let entropia = tamanhoSenha * Math.log2(tamanhoAlfabeto);
-
 const valorEntropia = document.querySelector('.entropia');
-valorEntropia.textContent = 2**Math.floor(entropia)/(100e6*60*60*24);
+    valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/(100e6*60*60*24)) + " dias para descobrir essa senha.";
+
